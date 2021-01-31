10be21d9308080cd6bf2e6ba1243cabd2a812c5a
Sergio Nemecio Ordoñez Argueta <sordonez88@gmail.com>
2021-01-30 22:02:40 -0600

    Added statistics endpoint logic, added Statistic model, added populate init data logic

--- src/controllers/statisticController.ts
+++ src/controllers/statisticController.ts
@@ -1,13 +1,90 @@
 import express from 'express';
+import axios from 'axios';
+import Statistic from '../models/Statistic';
+import { STATISTICS_ENDPOINT } from '../constants/endpoint';
+
+//Fetch Data from RAPID API
+const fetchInitData = async () => {
+  const apiEndpoint = `https://${process.env.COVID_API_HOST}${STATISTICS_ENDPOINT}`;
+  const apiKey = process.env.COVID_API_KEY;
+
+  const { data } = await axios.get(apiEndpoint, {
+    headers: {
+      'x-rapidapi-key': apiKey,
+      'x-rapidapi-host': process.env.COVID_API_HOST,
+    },
+  });
+
+  return data;
+};
+
+export const populateStatistics = async (resetData: boolean = false) => {
+  if (resetData) {
+    await Statistic.deleteMany();
+  } else {
+    const statistics = await Statistic.findOne();
+    // Data exists, no need to populate.
+    if (statistics) {
+      return;
+    }
+  }
+
+  try {
+    // Fetch data from RAPID API
+    const data = await fetchInitData();
+
+    // Iterate through each record
+    Object.values(data.response).forEach((stat: any) => {
+      const {
+        continent,
+        country,
+        population,
+        cases: { new: newers, active, critical, recovered, '1M_pop': M_POP, total },
+        deaths: { new: newers_death, '1M_pop': M_POP_death, total: total_death },
+        tests: { '1M_pop': M_POP_test, total: total_test },
+        time,
+      } = stat;
+
+      // create Statistic Object
+      const newStat = {
+        continent,
+        country,
+        population,
+        cases: { newers: +newers, active, critical, recovered, M_POP: +M_POP, total },
+        deaths: { newers: +newers_death, M_POP: +M_POP_death, total: +total_death },
+        tests: { M_POP: +M_POP_test, total: +total_test },
+        day: time,
+      };
+
+      //Persist Statistic Object
+      Statistic.create(newStat);
+    });
+  } catch (err) {
+    console.log(err.message);
+    throw new Error('Unable to retrieve data from API');
+  }
+};
 
 export const statistics_get = async (req: express.Request, res: express.Response) => {
-  res.send({});
+  const statistics = await Statistic.find().lean();
+  res.send({ statistics });
 };
 
 export const statistics_by_country_get = async (req: express.Request, res: express.Response) => {
-  res.send({});
+  const countryId = req.params.countryId;
+  const statistic = await Statistic.findOne({ country: countryId }).lean();
+
+  if (!statistic) {
+    res.status(403).json({
+      message: `Record not found for country: ${countryId}`,
+    });
+  }
+
+  res.send({ statistic });
 };
 
 export const sync_get = async (req: express.Request, res: express.Response) => {
-  res.send({});
+  populateStatistics(true)
+    .then(() => res.send({ sync: true }))
+    .catch((err) => res.status(403).json({ message: err.message }));
 };
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import { AUTH_ENDPOINT } from '../constants/endpoint';
 import { signup_post, login_post, logout_get } from '../controllers/authController';
 
 const router = Router();
 
-router.post('/signup', signup_post);
-router.post('/login', login_post);
-router.get('/logout', logout_get);
+router.post(`${AUTH_ENDPOINT}/signup`, signup_post);
+router.post(`${AUTH_ENDPOINT}/login`, login_post);
+router.get(`${AUTH_ENDPOINT}/logout`, logout_get);
 
 export default router;
