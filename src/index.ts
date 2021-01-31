139dd2ed920aa67e0c86d714023c385405ce1206
Sergio Nemecio Ordoñez Argueta <sordonez88@gmail.com>
2021-01-30 22:02:40 -0600

    Added statistics endpoint logic, added Statistic model, added populate init data logic

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import 'dotenv/config';
+import mongoose from 'mongoose';
 import app from './server';
+import { populateStatistics } from './controllers/statisticController';
 
-// Start the application by listening to specific port
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.info('COVID 19 statistics server start on port: ' + port);
-});
+// database connection
+const dbUser = process.env.USER_DB;
+const dbPwd = process.env.USER_PWD;
+const dbName = process.env.DB_NAME;
+const dbHost = process.env.DB_HOST;
+const dbURI = `mongodb+srv://${dbUser}:${dbPwd}@${dbHost}/${dbName}?retryWrites=true&w=majority`;
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(async () => {
+    //Populate Initial Data
+    await populateStatistics().catch((err) => console.log(err.message));
+
+    // Start the application by listening to specific port
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+      console.info('COVID 19 statistics server started on port: ' + port);
+    });
+  })
+  .catch((err) => console.log(err));
---
+++ src/models/Statistic.ts
@@ -0,0 +1,64 @@
+import mongoose, { Schema, Model, Document } from 'mongoose';
+
+interface Cases extends Document {
+  newers: number;
+  active: number;
+  critical: number;
+  recovered: number;
+  M_POP: number;
+}
+
+interface Deaths extends Document {
+  newers: number;
+  M_POP: number;
+  total: number;
+}
+
+interface Tests extends Document {
+  M_POP: number;
+  total: number;
+}
+
+export interface IStatistics extends Document {
+  continent: string;
+  country: string;
+  population: number;
+  cases: Cases;
+  deaths: Deaths;
+  tests: Tests;
+  day: Date;
+}
+
+const statisticSchema: Schema = new Schema(
+  {
+    continent: String,
+    country: {
+      type: String,
+      required: true,
+    },
+    population: Number,
+    cases: {
+      newers: Number,
+      active: Number,
+      critical: Number,
+      recovered: Number,
+      M_POP: Number,
+      total: Number,
+    },
+    deaths: {
+      newers: Number,
+      M_POP: Number,
+      total: Number,
+    },
+    tests: {
+      M_POP: Number,
+      total: Number,
+    },
+    day: Date,
+  },
+  { timestamps: true }
+);
+
+const Statistic: Model<IStatistics> = mongoose.model<IStatistics>('statistic', statisticSchema);
+
+export default Statistic;
