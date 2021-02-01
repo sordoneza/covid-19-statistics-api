7597cd5317078be6cf6f2a26bf0578f5ee59a936
Sergio Nemecio Ordoñez Argueta <sordonez88@gmail.com>
2021-01-31 23:14:48 -0600

    addedd endpoint to update an statistic record

--- src/routes/statisticsRoutes.ts
+++ src/routes/statisticsRoutes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import { STATISTICS_ENDPOINT } from '../constants/endpoint';
-import { statistics_get, statistics_by_country_get } from '../controllers/statisticController';
+import {
+  statistics_get,
+  statistics_by_country_get,
+  statistics_put,
+} from '../controllers/statisticController';
 import { requireAuth } from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.get(STATISTICS_ENDPOINT, requireAuth, statistics_get);
 router.get(`${STATISTICS_ENDPOINT}/:countryId`, requireAuth, statistics_by_country_get);
+router.put(`${STATISTICS_ENDPOINT}`, requireAuth, statistics_put);
 
 export default router;
