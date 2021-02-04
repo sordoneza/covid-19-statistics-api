2007f680a600188834f4eabb815a160405cf3fe7
Sergio Nemecio Ordoñez Argueta <sordonez88@gmail.com>
2021-02-03 22:19:45 -0600

    Added Swagger for api documentation

---
+++ src/swaggerSpec.ts
@@ -0,0 +1,32 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'COVID 19 Statistics API',
+    version: '1.0.0',
+    description:
+      'Covid 19 Statistics api provides data about the progress of the coronavirus around the world.',
+
+    contact: {
+      name: 'Sergio Ordoñez',
+    },
+  },
+  servers: [
+    {
+      url: 'http://localhost:4000/api',
+      description: 'Development server',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+
+  // Path to the API docs
+  apis: ['src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export default swaggerSpec;
